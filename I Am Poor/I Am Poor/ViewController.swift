8e084cb92483f79a49d475c7e24a160087908437
Lucas Manuel Ferrer <lucasferrer10@yahoo.com>
2020-04-10 18:14:39 -0400

    Add files via upload

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by LucasManuel on 4/10/20.
+//  Copyright © 2020 Lucas Manuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
